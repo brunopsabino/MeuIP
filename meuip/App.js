41f0ab3c30afd896d2d6fa962ba0a40e0c8669f6
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-08-06 01:14:05 -0300

    Layout e requisição do IP

--- meuip/App.js
+++ meuip/App.js
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import logo from './assets/logo.png'
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.findMyIp = this.findMyIp.bind(this)
+    this.state = {
+      data: ''
+    }
+  }
+
+  async findMyIp() {
+    this.setState({
+      data: 'Descobrindo IP...'
+    })
+    const ip = await fetch('http://httpbin.org/ip')
+    const data = await ip.json()
+      let test = data.origin.split(',')
+    this.setState({
+      data: test[0]
+    })
+
+  }
+
+  render (){
+    return (
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Image source={logo}/>
+          <Text style={styles.ip}>{this.state.data}</Text>
+          <Button title='Descobrir meu IP!' onPress={this.findMyIp} />
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.made}>
+          Desenvolvido por Bruno Sabino
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#4e3959',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-});
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  ip: {
+    color: 'white',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  footer: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  made: {
+    color: 'white',
+    textAlign: 'center'
+  }
+})
